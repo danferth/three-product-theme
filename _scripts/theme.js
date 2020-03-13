d437bdfc67c32d2ea2693a3559d1c5cbf67ec927
danferth <danferth@gmail.com>
2020-03-12 17:32:50 -0700

    js for more buton and product images

--- _scripts/theme.js
+++ _scripts/theme.js
@@ -9,8 +9,45 @@ $(document).ready(function(){
 var prodID = $('.product_description').attr('data-description');
 var moreID = $('.product_description_more').attr('data-more');
 
-console.log(prodID);
-console.log(moreID);
+$('.product_description_more').on('click', function(e){
+    if(prodID === moreID){
+        if($('.product_description_more i').hasClass('fa-arrow-alt-circle-down')){
+            $('.product_description').css({
+                'overflow' : 'visible',
+                'height' : 'auto'});
+
+            $('.product_description_more i')
+            .removeClass('fa-arrow-alt-circle-down')
+            .addClass('fa-arrow-alt-circle-up')
+            .siblings('.moreLess').html('less');
+        }else{
+            $('.product_description').css({
+                'overflow' : 'hidden',
+                'height' : 20 + 'rem'});
+
+            $('.product_description_more i')
+            .removeClass('fa-arrow-alt-circle-up')
+            .addClass('fa-arrow-alt-circle-down')
+            .siblings('.moreLess').html('more');
+        }
+    }
+});
+
+// product page images swap
+var productMainsrc = $('.product_main_featured_img').attr('src');
+
+var backToMainImg = function(mainSrc){
+        $('.product_main_featured_img').attr('src', mainSrc);
+};
+
+$('.product_main_featured_img').on('click', function(){
+    backToMainImg(productMainsrc);
+});
+
+$('.product_main_sub_img').on('click', function(e){
+    var source = $(this).attr('data-largesrc');
+    $('.product_main_featured_img').attr('src', source);
+});
 
 
 }); //END Doc.ready
